2d7443cfe61262274d269d9322c3bdcbb85bf3f8
BenGround <benjaminhartmann.contact@gmail.com>
2018-06-01 09:01:44 +0200

    Beginning of the FireBall class

---
+++ lorann/model/src/main/java/mobile/FireBall.java
@@ -0,0 +1,35 @@
+package mobile;
+
+import java.io.IOException;
+
+import model.IMap;
+import model.Permeability;
+import model.Sprite;
+
+public class FireBall extends Mobile {
+	
+	private static Sprite fireball;
+
+	public FireBall(int x, int y, Sprite sprite, IMap map, Permeability permeability) throws IOException {
+		super(x, y, sprite, map, permeability);
+		FireBall.setFireball(sprite);
+		fireball.loadImage();
+	}
+
+	public static Sprite getFireball() {
+		return fireball;
+	}
+
+	public static void setFireball(Sprite monster1) {
+		FireBall.fireball = monster1;
+	}
+
+	@Override
+	public Boolean victory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
